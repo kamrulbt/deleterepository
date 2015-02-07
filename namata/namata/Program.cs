17e23725c49bd432253f228fbe5e6daee07d3090
kamrulbt <kamrulbt@gmail.com>
2015-02-07 12:23:06 -0800

    comitted by hasan 4

--- namata/namata/Program.cs
+++ namata/namata/Program.cs
@@ -10,7 +10,7 @@ namespace namata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("change by hasan3 ");
+            Console.WriteLine("change by hasan4 ");
             int upto = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\n you gave {0}  ",upto);
             int check = 1,output=1;
