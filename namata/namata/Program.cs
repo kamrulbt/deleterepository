23c06319292ec828c509eab20bfa9b7da2bf5c89
kamrulbt <kamrulbt@gmail.com>
2015-02-07 01:19:42 -0800

    just checkin

---
+++ namata/namata/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" please enter your target : \n");
+            int upto = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n you gave {0}  ",upto);
+            int check = 1,output=1;
+            for (int i = 1; i <= upto;i++ )
+            {
+                check = 1;
+                for (int j = 1; j <= 10;j++ )
+                {
+                    if(check == 1){
+                        Console.WriteLine(i+ " er namata\n");
+                        check = 0;
+                    }
+                    output = i * j;
+                    Console.WriteLine("{0} * {1} = {2}",i,j,output);
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
